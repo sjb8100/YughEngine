4e7abcfd0052737757e817a2e91a9bdd08e03aa4
OFFICE-1\johnb <johnb@odplot.com>
2018-06-26 03:26:22 -0500

    Setup module and added discord sdk

---
+++ modules/discord/register_types.cpp
@@ -0,0 +1,12 @@
+#include "register_types.h"
+#include "class_db.h"
+#include "engine.h"
+
+void register_godotsteam_types() {
+	// Inculde new CPP files here to "register" them with ClassDB::register_class<CLASS>();
+	// Check here http://docs.godotengine.org/en/stable/development/cpp/custom_modules_in_cpp.html
+}
+
+void unregister_godotsteam_types(){
+
+}
