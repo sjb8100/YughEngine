f904a1fa1b9b254c1490a8028f519fed5cc28028
OFFICE-1\johnb <johnb@odplot.com>
2018-06-27 15:22:04 -0500

    Added discord build support for osx and win32

--- modules/discord/config.py
+++ modules/discord/config.py
@@ -1,11 +1,20 @@
 def can_build(env, platform):
-    return True
+    return platform=="x11" or platform=="windows" or platform=="osx"
 
 def configure(env):
     env.Append(CPPPATH="#modules/discord/sdk/win64/include/")
 
-    # Only works for windows on the VS prompt, but it's all i'm testing atm
-    env.Append(LINKFLAGS=["discord-rpc.lib"])
-    env.Append(LIBPATH="#modules/discord/sdk/win64/lib")
-    
-    
+    if env["platform"] == "windows":
+        if env["bits"]=="32":
+            env.Append(LIBPATH="#modules/discord/sdk/win32/lib")
+        else: # 64 bit
+            env.Append(LIBPATH="#modules/discord/sdk/win64/lib")
+
+        if env["CC"] == "cl": # Visual Studio builder support
+            env.Append(LINKFLAGS="discord-rpc.lib") 
+        else: # gcc support
+            env.Append(LIBS="discord-rpc")
+
+    elif env["platform"] == "osx":
+        env.Append(LIBS="libdiscord-rpc")
+        env.Append(LIBPATH="#modules/discord/sdk/osx/lib")
